c28f0ffb116b3add36e64c02a092209ad0c3d490
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-13 08:29:31 -0700

    stackmybchup

--- src/App.js
+++ src/App.js
@@ -177,6 +177,12 @@ class App extends Component {
               </p>
             </div>
             <div className="pure-u-1 pure-u-md-1-3">
+              <p className='branding'>
+                <a href='https://twitter.com/StackmyBCHup'>
+                  <img src="./StackmyBCHup.jpg" /><br />
+                  StackmyBCHup
+                </a>
+              </p>
             </div>
             <div className="pure-u-1 pure-u-md-1-3">
               <p>
